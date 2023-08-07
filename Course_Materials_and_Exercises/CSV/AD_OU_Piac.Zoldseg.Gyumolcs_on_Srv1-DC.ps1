cc4f1e2fc3a21ff03ed685eec0f5194cb1562c91
kristof-levai <kristof.levai@gmail.com>
2023-08-07 13:31:23 +0200

    Add files via upload

---
+++ Course_Materials_and_Exercises/CSV/AD_OU_Piac.Zoldseg.Gyumolcs_on_Srv1-DC.ps1
@@ -0,0 +1,13 @@
+﻿$server = "Srv1-dc"
+
+$ou1 = "Piac"
+$ou2 = "Zoldseg"
+$ou3 = "Gyumolcs"
+
+$dc1 = "gyakorlat"
+$dc2 = "local"
+
+New-ADOrganizationalUnit -Name:"$ou1" -Path:"DC=$dc1,DC=$dc2" -ProtectedFromAccidentalDeletion:$false -Server:"$server.$dc1.$dc2"
+
+New-ADOrganizationalUnit -Name:"$ou2" -Path:"OU=$ou1,DC=$dc1,DC=$dc2" -ProtectedFromAccidentalDeletion:$false -Server:"$server.$dc1.$dc2"
+New-ADOrganizationalUnit -Name:"$ou3" -Path:"OU=$ou1,DC=$dc1,DC=$dc2" -ProtectedFromAccidentalDeletion:$false -Server:"$server.$dc1.$dc2"
---
+++ Course_Materials_and_Exercises/CSV/_ad_eloleptetes.gyakorlat.local.ps1
@@ -0,0 +1,18 @@
+﻿#
+# Windows PowerShell-parancsfájl az Active Directory tartományi szolgáltatások AD DS központi telepítéséhez
+#
+
+Import-Module ADDSDeployment
+Install-ADDSForest `
+-CreateDnsDelegation:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "WinThreshold" `
+-DomainName "gyakorlat.local" `
+-DomainNetbiosName "gyakorlat" `
+-ForestMode "WinThreshold" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
+
